5734764805766a4b985155f082165a895c03af81
Evgeniy S. Yakushenko <yes@incart.ru>
2021-07-30 14:08:10 +0300

    intro

---
+++ Intro/vue_proj/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        sdfs {{1}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                a:1
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .container{
+        color: red;
+    }
+</style>
